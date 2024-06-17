ef8063cef6c739c235f2f79664e55ee3aa42686f
André Santos <dos.santos.andre@outlook.pt>
2024-06-18 00:29:47 +0200

    add vertical mobile virtual slide styles

--- src/routes/project/horizontal-virtual-slide.lazy.tsx
+++ src/routes/project/horizontal-virtual-slide.lazy.tsx
@@ -3,6 +3,7 @@ import { useVirtualizer, Virtualizer } from "@tanstack/react-virtual";
 import classNames from "classnames";
 import debounce from "lodash/debounce";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useWindowSize } from "usehooks-ts";
 
 export const Route = createLazyFileRoute("/project/horizontal-virtual-slide")({
   component: HorizontalVirtualSlide,
@@ -29,24 +30,31 @@ function HorizontalVirtualSlide() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const parentRef = useRef<HTMLDivElement>(null);
   const length = 50; // Number of columns
-  const minPaddingX = 100; // Minimum padding in px
-  const bottomPadding = 80; // Bottom padding in px (also used for info height)
+  const minPadding = 100; // Minimum padding in px
+  const bottomPadding = 112; // Bottom padding in px (also used for info height)
   const isFirst = currentIndex === 0;
   const isLast = currentIndex === length - 1;
 
-  const estimatedSize = getMinRatioWidth(
-    "16:9",
-    parentRef.current
-      ? {
-          innerHeight: parentRef.current.clientHeight - bottomPadding,
-          innerWidth: parentRef.current.clientWidth - minPaddingX * 2,
-        }
-      : undefined,
-  );
+  const { width: windowWidth, height: windowHeight } = useWindowSize({
+    debounceDelay: 250,
+  });
+  const isSmallScreen = windowWidth < 768;
 
-  const paddingX = useMemo(
-    () => (window.innerWidth - estimatedSize) / 2,
-    [estimatedSize],
+  const estimatedSize = !isSmallScreen
+    ? getMinRatioWidth(
+        "16:9",
+        parentRef.current
+          ? {
+              innerHeight: parentRef.current.clientHeight - bottomPadding,
+              innerWidth: parentRef.current.clientWidth - minPadding * 2,
+            }
+          : undefined,
+      )
+    : (windowWidth * 9) / 16 + bottomPadding;
+
+  const padding = useMemo(
+    () => ((!isSmallScreen ? windowWidth : windowHeight) - estimatedSize) / 2,
+    [estimatedSize, isSmallScreen, windowHeight, windowWidth],
   );
 
   // Debounced function to update current index
@@ -67,31 +75,31 @@ function HorizontalVirtualSlide() {
     return debouncedUpdateCurrentIndex.cancel();
   }, [debouncedUpdateCurrentIndex]);
 
-  const columnVirtualizer = useVirtualizer({
-    horizontal: true,
+  const virtualizer = useVirtualizer({
+    horizontal: !isSmallScreen,
     count: length,
     getScrollElement: () => parentRef.current,
     estimateSize: () => estimatedSize,
     overscan: 5, // Sweet spot for quick swipe scroll
-    paddingStart: paddingX,
-    paddingEnd: paddingX,
-    scrollPaddingStart: paddingX,
-    scrollPaddingEnd: paddingX,
+    paddingStart: isSmallScreen ? padding + bottomPadding / 2 : padding,
+    paddingEnd: padding,
+    scrollPaddingStart: padding,
+    scrollPaddingEnd: padding,
     onChange: debouncedUpdateCurrentIndex,
   });
 
   useEffect(() => {
-    const recalculateColumnWidths = () => columnVirtualizer.measure();
+    const recalculateColumnWidths = () => virtualizer.measure();
     const debouncedResizeHandler = debounce(recalculateColumnWidths, 250);
     window.addEventListener("resize", debouncedResizeHandler);
     return () => window.removeEventListener("resize", debouncedResizeHandler);
-  }, [columnVirtualizer]);
+  }, [virtualizer]);
 
   const handleSmoothScroll = useCallback(
     (index: number) => {
-      columnVirtualizer.scrollToIndex(index, { behavior: "smooth" });
+      virtualizer.scrollToIndex(index, { behavior: "auto" });
     },
-    [columnVirtualizer],
+    [virtualizer],
   );
 
   const handlePrev = useCallback(() => {
@@ -107,17 +115,24 @@ function HorizontalVirtualSlide() {
   }, [currentIndex, handleSmoothScroll]);
 
   return (
-    <main className="pt-16">
+    <main className={classNames({ "pt-16": !isSmallScreen })}>
       <h1 className="text-3xl font-bold text-center py-2">
         Horizontal Virtual Slide
       </h1>
-      <div className="absolute top-0 bottom-0 mt-32 mb-8 flex flex-col justify-center z-0">
+      <div
+        className={classNames(
+          "absolute top-0 bottom-0 flex flex-col justify-center z-0",
+          {
+            "mt-32 mb-8": !isSmallScreen,
+          },
+        )}
+      >
         {/* Previous button */}
         {!isFirst && (
           <button
             onClick={handlePrev}
             className="absolute top-1/2 left-0 transform -translate-y-1/2 text-white text-xl p-4 bg-zinc-700 z-10"
-            style={{ marginTop: -(bottomPadding / 2) }}
+            style={{ marginTop: !isSmallScreen ? -(bottomPadding / 2) : 0 }}
           >
             ◀️
           </button>
@@ -126,33 +141,58 @@ function HorizontalVirtualSlide() {
         <div
           ref={parentRef}
           id="scrollableElement"
-          className="snap-x snap-mandatory overflow-x-auto no-scrollbar w-screen h-full"
+          className={classNames(
+            "snap-mandatory overflow-x-auto no-scrollbar w-screen h-full",
+            {
+              "snap-y overflow-y-auto": isSmallScreen,
+              "snap-x overflow-x-auto": !isSmallScreen,
+            },
+          )}
         >
           <div
-            className="relative top-1/2 transform -translate-y-1/2"
-            style={{
-              height: `${(estimatedSize / 16) * 9 + bottomPadding}px`, // 80px for info
-              width: `${columnVirtualizer.getTotalSize()}px`,
-            }}
+            className={classNames("relative", {
+              "top-1/2 transform -translate-y-1/2": !isSmallScreen,
+            })}
+            style={
+              !isSmallScreen
+                ? {
+                    height: `${(estimatedSize / 16) * 9 + bottomPadding}px`, // 80px for info
+                    width: `${virtualizer.getTotalSize()}px`,
+                  }
+                : {
+                    width: `100vw`,
+                    height: `${virtualizer.getTotalSize() + bottomPadding}px`,
+                  }
+            }
           >
-            {columnVirtualizer.getVirtualItems().map((virtualColumn) => {
-              const isCurrent = virtualColumn.index === currentIndex;
+            {virtualizer.getVirtualItems().map((virtualItem) => {
+              const isCurrent = virtualItem.index === currentIndex;
 
               return (
                 <div
-                  key={virtualColumn.index}
-                  className={"absolute top-0 left-0 snap-center aspect-video"}
-                  style={{
-                    width: `${virtualColumn.size}px`,
-                    transform: `translateX(${virtualColumn.start}px)`,
-                  }}
+                  key={virtualItem.index}
+                  className={classNames(
+                    "absolute top-0 left-0 snap-center aspect-video",
+                  )}
+                  style={
+                    !isSmallScreen
+                      ? {
+                          width: `${virtualItem.size}px`,
+                          transform: `translateX(${virtualItem.start}px)`,
+                        }
+                      : {
+                          width: `100%`,
+                          transform: `translateY(${virtualItem.start}px)`,
+                        }
+                  }
                   {...(!isCurrent && { "aria-hidden": true, inert: "true" })}
                 >
                   <div
                     className={classNames(
-                      "w-full h-full transition-all duration-300 delay-150 ease-in-out",
+                      "w-full h-full transition-all duration-300 ease-in-out",
                       {
-                        "scale-90 transform": !isCurrent,
+                        "scale-90 transform": !isCurrent && !isSmallScreen,
+                        "opacity-70": !isCurrent && isSmallScreen,
                       },
                     )}
                   >
@@ -163,7 +203,7 @@ function HorizontalVirtualSlide() {
                         "w-full h-full bg-black flex items-center justify-center text-white text-2xl font-bold",
                       )}
                     >
-                      Column {virtualColumn.index}
+                      Item {virtualItem.index}
                     </div>
                     {/* Info */}
                     <div className="max-h-20 flex flex-row flex-nowrap">
@@ -207,7 +247,7 @@ function HorizontalVirtualSlide() {
           <button
             onClick={handleNext}
             className="absolute top-1/2 right-0 transform -translate-y-1/2 text-white text-xl p-4 bg-zinc-700 z-10"
-            style={{ marginTop: -(bottomPadding / 2) }}
+            style={{ marginTop: !isSmallScreen ? -(bottomPadding / 2) : 0 }}
           >
             ▶️
           </button>
